275ecf1bdafec8fb33f3a6342a3e2cbc24446fe1
Vyacheslav Yarkin <yarkin.vyacheslav@mail.ru>
2020-09-08 00:30:09 +0300

    How to run -> Readme, major edits

--- seminar2/entrypoint.sh
+++ seminar2/entrypoint.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-printf 'Neurodocker startup - success'
+printf 'Neurodocker startup - success\n'
 
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
-printf 'FREESURFER - check' 
+printf 'FREESURFER - check\n' 
 
 ./opt/miniconda-latest/envs/neuro/bin/jupyter-lab --ip=0.0.0.0 --port=8080 --no-browser --allow-root --notebook-dir='/workspace' --NotebookApp.token=''
